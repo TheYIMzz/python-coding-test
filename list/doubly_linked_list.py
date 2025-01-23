5b0a31e3f1de6206b6fdd5439bf89a4224a84261
TheYIM - Surface <yim9412@gmail.com>
2025-01-23 16:46:33 +0900

    이름 변경

---
+++ list/doubly_linked_list.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self):
+        self.value = None
+        self.pre_node = None
+        self.post_node = None
+
+
+class DoublyLinkedList:
+    def append(self, idx, value):
+        pass
+
+
+    def get(self, idx, value):
+        pass
+
+
+    def insert(self, idx, value):
+        pass
+
+
+    def delete(self, idx, value):
+        pass
