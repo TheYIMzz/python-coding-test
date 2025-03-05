c0ac6c4b6c9b238970358961cf9772ca07887f71
TheYIM-Home <yim9412@gmail.com>
2025-03-05 22:23:57 +0900

    backtrack 수열

--- exhaustive_search/backtrack_sequence.py
+++ exhaustive_search/backtrack_sequence.py
@@ -1,19 +1,16 @@
 """
-
     백트랙킹(Back Tracking)
         solution이 될 가능성이 없는 candidate는 더 이상 탐색하지 않고
         candidate를 포기(backtrack)하면서 탐색
 
     nums = [1, 2, 3, 4]로 만들 수 있는 모든 순열을 반환하시오
-
-
 """
 
+# 순열
 def permute(nums):
 
     def backtrack(curr):
-
-        if len(curr) == len(nums):
+        if len(curr) == len(nums): # base case
             ans.append(curr[:])
             return
 
