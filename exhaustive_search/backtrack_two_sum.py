bde51cca0b121d7657f953bccd5d7c54ff7338ef
TheYIM-Home <yim9412@gmail.com>
2025-05-02 17:00:19 +0900

    back_track two sum

--- exhaustive_search/backtrack_two_sum.py
+++ exhaustive_search/backtrack_two_sum.py
@@ -3,30 +3,29 @@
 """
 
 nums = [4, 9, 7, 5, 1]
-target = 12
+target = 15
 
-def two_sum(nums, target):
+def two_sum():
 
-    def backtrack(start, curr):
-        # base case
-        if len(curr) == 2 and sum(nums[i] for i in curr) == target:
+    def back_track(start, curr):
+
+        if len(curr) == 3 and sum(nums[i] for i in curr) == target:
             return curr
 
-        # print('start: ', start)
         for i in range(start, len(nums)):
             curr.append(i)  # 인덱스 추가
             print('현재 인덱스:', i)
-            backtrack_result = backtrack(i + 1, curr)
+            backtrack_result = back_track(i + 1, curr)
             print('curr: ', curr)
             print('backtrack_result: ', backtrack_result, i)
-            if backtrack_result:  # 재귀 호출에서 두 수의 인덱스 리스트)을 찾았다면
+
+            if backtrack_result:  # 재귀 호출결과에서 두 수의 인덱스 리스트를 찾았다면
                 print('두 수 합 찾은 결과: ', backtrack_result)
                 return backtrack_result
 
             pop_num = curr.pop()
             print('pop_num: ', pop_num)
-        return None
 
-    return backtrack(0, [])
+    return back_track(0, [])
 
-print(two_sum(nums, target))
+print(two_sum())
