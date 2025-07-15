2f64de677730fc8ca1f3085a10aa5e912173244f
TheYIM-Home <yim9412@gmail.com>
2025-07-15 22:27:45 +0900

    임시

--- a.py
+++ a.py
@@ -1,38 +1,27 @@
-
-graph = {
-    1: [(2, 2), (1, 4)],
-    2: [(1, 3), (9, 5), (6, 6)],
-    3: [(4, 6)],
-    4: [(3, 3), (5, 7)],
-    5: [(1, 8)],
-    6: [(3, 5)],
-    7: [(7, 6), (9, 8)],
-    8: [],
-}
+def main():
 
 
+    result = []
+    def back_track(start, curr):
 
-import heapq
+        print(curr)
 
+        if len(curr) == 5:
+            if curr[0] == 'U' and curr[:-1] == 'U':
+                return
+            curr.clear()
 
-def dijkstra(graph, start, final):
+        result.append(''.join(curr))
 
-    costs = {}
-    pq = [] 
+        for i in range(start, 5):
+            curr.append(vowels[i])
+            back_track(i + 1, curr)
+            curr.pop()
 
-    heapq.heappush(pq, (0, start))
-
-    while pq:
-        cur_cost, cur_node = heapq.heappop(pq)
-
-        if cur_node not in costs:
-            costs[cur_node] = cur_cost
-
-            for cost, next_node in graph[cur_node]:
-                next_cost = cur_cost + cost
-                heapq.heappush(pq, (next_cost, next_node))
-
-    return costs[final]
+    back_track(0, [])
+    print(result)
 
 
-print(dijkstra(graph, 1, 8))
+word = "AAAAE"
+vowels = ['A', 'E', 'I', 'O', 'U']
+print(main())
