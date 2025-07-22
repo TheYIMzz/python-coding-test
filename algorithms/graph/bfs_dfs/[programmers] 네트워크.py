14e7931be32ae639ca493e2c7e4544bf96729eb8
TheYIM-Surface <yim9412@gmail.com>
2025-07-22 14:19:22 +0900

    네트워크

---
+++ algorithms/graph/bfs_dfs/[programmers] 네트워크.py
@@ -0,0 +1,36 @@
+
+from collections import deque
+
+def main():
+
+    for idx, c in enumerate(computers):
+        print(idx, c)
+
+
+    visited = [False] * n
+
+    def bfs(start):
+        print('start: ', start)
+        queue = deque()
+        queue.append(start)
+        visited[start] = True
+
+        while queue:
+            cur_v = queue.popleft()
+
+            for j in range(n):
+                if not visited[j] and computers[cur_v][j] == 1:
+                    visited[j] = True
+                    queue.append(j)
+
+
+    network_cnt = 0
+    for i in range(n):
+        if not visited[i]:
+            bfs(i)
+            network_cnt += 1
+
+    return network_cnt
+n = 3   # 컴퓨터 개수
+computers = [[1, 1, 0], [1, 1, 1], [0, 1, 1]] # 연결정보
+print(main())
