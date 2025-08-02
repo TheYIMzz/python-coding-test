7d546b0f13353b66afc5774374cac58ce8a5857e
TheYIM-Home <yim9412@gmail.com>
2025-08-02 18:05:43 +0900

    단어 변환

--- algorithms/graph/bfs_dfs/[programmers] 단어 변환.py
+++ algorithms/graph/bfs_dfs/[programmers] 단어 변환.py
@@ -3,25 +3,26 @@ from collections import deque
 
 def solution(begin, target, words):
 
-    def bfs(start_v, depth):
+    def bfs(begin, depth):
         visited = set()
         queue = deque()
-        queue.append((start_v, depth))
+
+        queue.append((begin, depth))
 
         while queue:
-            cur_v, cur_dep = queue.popleft()
-
-            if cur_v == target:
-                return cur_dep
+            cur_v, cur_depth = queue.popleft()
+            print(cur_v, cur_depth)
+            if target == cur_v:
+                return cur_depth
 
             for word in words:
                 if word not in visited and is_good(cur_v, word):
                     visited.add(word)
-                    queue.append((word, cur_dep + 1))
+                    queue.append((word, cur_depth + 1))
 
-        return depth
+        return 0
 
-    # 한 글자만 다른지 검사
+    # 한글자만 다른지 검사 함수
     def is_good(w1, w2):
         count = 0
         for a, b in zip(w1, w2):
@@ -31,11 +32,9 @@ def solution(begin, target, words):
             if count > 1:
                 return False
 
-        return count == 1
+        return count == 1  # 정확히 1글자 차이만 True 반환 (같은 단어되는 것 허용 방지)
 
-
-    answer = bfs(begin, 0)
-    return answer
+    return bfs(begin, 0)
 
 
 
