9773f315b51e591e19de077f0321ab1ae4bfaf10
TheYIM-Home <yim9412@gmail.com>
2025-08-02 17:17:13 +0900

    타겟 남바 dfs

--- algorithms/graph/bfs_dfs/[programmers] 타겟 넘버 bfs.py
+++ algorithms/graph/bfs_dfs/[programmers] 타겟 넘버 bfs.py
@@ -1,7 +1,8 @@
+
+#######################################
+## bfs
 from collections import deque
 
-
-# bfs
 def solution(numbers, target):
     queue = deque()
     queue.append((0, 0))  # (index, 현재까지 합)
@@ -29,11 +30,9 @@ target = 4
 print(solution(numbers, target))
 
 #######################################
-
-# dfs
+## dfs
 def solusuion_2(numbers, target):
     count = 0
-
 
     def dfs(idx, total):
         nonlocal count
