f22f90cca3627b855a26a37b985fd2cc6db14747
TheYIM-Surface <yim9412@gmail.com>
2025-09-08 15:00:47 +0900

    k번째수

--- algorithms/sort/[programmers] K번째수.py
+++ algorithms/sort/[programmers] K번째수.py
@@ -1,16 +1,20 @@
 array =  [1, 5, 2, 6, 3, 7, 4]
 commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
-
 def main():
-
     answer = []
+
 
     for command in commands:
         i, j, k = command
+        new_array = []
 
-        sliced = sorted(array[i-1:j])
-        answer.append(sliced[k - 1])
+        for t in range(i-1, j):
+            new_array.append(array[t])
+
+        new_array.sort()
+        answer.append(new_array[k-1])
+
 
     return answer
 
