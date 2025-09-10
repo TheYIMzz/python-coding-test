ac0c8d19a76983cec514fbc7bd8405fda75ef174
TheYIM-Surface <yim9412@gmail.com>
2025-09-10 17:50:00 +0900

    가장 먼 노드

--- algorithms/graph/bfs_dfs/[programmers] 가장 먼 노드.py
+++ algorithms/graph/bfs_dfs/[programmers] 가장 먼 노드.py
@@ -1,31 +1,27 @@
 from collections import deque
 
-
 def main():
 
     graph = [[] for _ in range(n + 1)]
-    visited = [False] * (n + 1)
-    dist = [0] * (n + 1)
 
     for u, v in vertex:
         graph[u].append(v)
         graph[v].append(u)
 
-    queue = deque()
-    queue.append(1)
-    visited[1] = True
+    visited = [-1] * (n + 1)
+    queue = deque([1])
+    visited[1] = 1
 
     while queue:
         cur_v = queue.popleft()
 
         for next_v in graph[cur_v]:
-            if not visited[next_v]:
-                visited[next_v] = True
-                dist[next_v] = dist[cur_v] + 1
+            if visited[next_v] == -1:
+                visited[next_v] = visited[cur_v] + 1
                 queue.append(next_v)
 
-    max_dist = max(dist)
-    return dist.count(max_dist)
+    max_depth = max(visited)
+    return visited.count(max_depth)
 
 n = 6
 vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
