a226b904eaa15892ab765e2a1ded9cc39ddb0dcb
TheYIM-Surface <yim9412@gmail.com>
2025-09-10 10:55:59 +0900

    -

--- algorithms/hash_table/[programmers] 전화번호 목록.py
+++ algorithms/hash_table/[programmers] 전화번호 목록.py
@@ -4,7 +4,7 @@ phone_book = ["119", "97674223", "1195524421"]
 def main():
 
     phone_book.sort()
-    for i in range(len(phone_book)):
+    for i in range(len(phone_book) -1):
 
         if phone_book[i + 1].startswith(phone_book[i]):
             return False
