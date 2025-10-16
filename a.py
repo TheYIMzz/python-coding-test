fda3c7e118ff0555b7f66acdd368e99bf0b59062
TheYIM-Surface <yim9412@gmail.com>
2025-10-16 17:42:25 +0900

    집가서 해야지

---
+++ a.py
@@ -0,0 +1,36 @@
+
+
+
+def solution():
+
+    tickets.sort()
+    visited = [False] * len(tickets)
+    answer = []
+
+    for t in tickets:
+        print(t)
+
+    def back_track(curr):
+        # print(curr, len(tickets), len(curr))
+        if len(tickets) == len(curr):
+            answer.append(curr[:])
+            return
+
+        for i in range(len(tickets)):
+            if not visited[i]:
+
+                if tickets[i][0] == curr[0]:
+                    print('tickets[i][0] == curr: ', tickets[i][0], curr[0], i)
+                    print('tickets[i][1]: ', tickets[i][1])
+                    curr.append(tickets[i][1])
+                    back_track(curr)
+                    curr.pop()
+
+    back_track([start])
+
+    return answer
+
+start = "ICN"
+tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+# tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+print(solution())
