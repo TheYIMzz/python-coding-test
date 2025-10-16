4149792cdcb10eb6a1611138fc9388de719bc70c
TheYIM-Surface <yim9412@gmail.com>
2025-10-16 17:24:35 +0900

    -

--- algorithms/graph/bfs_dfs/[programmers] 단어 변환.py
+++ algorithms/graph/bfs_dfs/[programmers] 단어 변환.py
@@ -3,41 +3,35 @@ from collections import deque
 
 def solution(begin, target, words):
 
-    def bfs(begin, depth):
-        visited = set()
-        queue = deque()
+    visited = set()
+    queue = deque()
+    queue.append((begin, 0))
 
-        queue.append((begin, depth))
-
-        while queue:
-            cur_v, cur_depth = queue.popleft()
-            print(cur_v, cur_depth)
-            if target == cur_v:
-                return cur_depth
-
-            for word in words:
-                if word not in visited and is_good(cur_v, word):
-                    visited.add(word)
-                    queue.append((word, cur_depth + 1))
-
-        return 0
-
-    # 한글자만 다른지 검사 함수
-    def is_good(w1, w2):
+    def check_words(w1, w2):
         count = 0
+
         for a, b in zip(w1, w2):
             if a != b:
                 count += 1
 
             if count > 1:
                 return False
-
         return count == 1  # 정확히 1글자 차이만 True 반환 (같은 단어되는 것 허용 방지)
 
-    return bfs(begin, 0)
+    while queue:
+        cur_word, depth = queue.popleft()
+        print(cur_word, depth)
 
+        if cur_word == target:
+            return depth
 
+        for next_word in words:
+            if next_word not in visited:
+                if check_words(cur_word, next_word):
+                    visited.add(next_word)
+                    queue.append((next_word, depth + 1))
 
+    return 0
 
 begin = 'hit'
 target = 'cog'
