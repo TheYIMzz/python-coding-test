1cd4d2ff79d7674ec6e0dd910d16731722c70c28
TheYIM-Home <yim9412@gmail.com>
2025-10-20 21:53:02 +0900

    2003

--- algorithms/two_pointer/beakjoon_2003.py
+++ algorithms/two_pointer/beakjoon_2003.py
@@ -30,25 +30,25 @@ def solution():
     N, M = map(int, sys.stdin.readline().strip().split())
     nums = list(map(int, sys.stdin.readline().strip().split()))
 
-    start = 0  # 왼쪽 포인터
-    end = 0    # 오른쪽 포인터
+    start = 0  # 왼쪽 포인터 -> i
+    end = 0    # 오른쪽 포인터 -> j
     total = 0  # 현재 구간 [start ~ end-1]의 합
     count = 0  # 조건 만족하는 구간 수
 
     while True:
-        # 1️⃣ 합이 아직 M보다 작을 때 → 오른쪽 확장
+        # 합이 아직 M보다 작을 때 → 오른쪽 확장
         if total < M:
             if end == N:  # 오른쪽 포인터가 끝까지 도달 → 확장 불가
                 break  # 전체 while 종료
             total += nums[end]  # A[end] 값을 더해서 구간 확장
             end += 1   # 오른쪽 포인터 한 칸 이동
 
-        # 2️⃣ 합이 딱 M일 때 → 카운트 +1, 왼쪽 줄이기
+        #  합이 딱 M일 때 → 카운트 +1, 왼쪽 줄이기
         elif total == M:
             count += 1            # 조건 만족하는 구간 발견
-            total -= nums[start]  # 왼쪽 값을 빼서 다음 구간으로 이동 준비
+            total -= nums[start]  # 왼쪽 값을 빼서 새로운 조합 준비  [(1, 1), 1, 1] -> [1, (1), 1, 1]
             start += 1            # 왼쪽 포인터 한 칸 이동
-        # 3️⃣ 합이 M보다 클 때 → 왼쪽 줄이기
+        #  합이 M보다 클 때 → 왼쪽 줄이기
         else:  # total > M
             total -= nums[start]   # 왼쪽 값을 빼서 합 줄이기
             start += 1          # 왼쪽 포인터 한 칸 이동
